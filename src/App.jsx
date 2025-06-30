6be46a78aae04a2b10b037527d221914d03b060a
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-06-30 18:01:10 +0600

    Header start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <div className="font-quickSand">
+        Hello world!
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
