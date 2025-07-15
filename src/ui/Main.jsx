4ccf58de5b713fd08e93a569279b8db1ba478038
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-07-15 22:25:35 +0600

    Event section

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -4,7 +4,7 @@ export default function Main({ children }) {
   return (
     <div
       className="text-gray-500
-  bg-gray-100 p-4 sm:ml flex gap-2 flex-col lg:flex-row traslate-all
+  bg-gray-100 p-4 sm:ml flex gap-5 flex-col lg:flex-row traslate-all
   duration-300 mt-14 dark:bg-gray-800"
     >
       {children}
